10fd66a10c96c5a30ee21fe7394c6ca058c4debf
PatrickNthiwa <pwnthiwa@gmail.com>
2023-05-05 19:24:26 +0300

    "c"

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\AdminController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -30,7 +31,7 @@ Route::middleware('auth')->group(function () {
 
 
 Route::middleware('auth','admin')->name('admin.')->prefix('admin')->group(function(){
-    Route::get('/',[AdminCon])
+    Route::get('/',[AdminController::class])
 });
 
 
