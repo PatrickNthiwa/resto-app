07d2f4495ce705cd9ad01e6c8bae5b86c0424966
PatrickNthiwa <pwnthiwa@gmail.com>
2023-05-05 19:21:33 +0300

    "x-admin-layout"

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -8,6 +8,6 @@ use Illuminate\Http\Request;
 class AdminController extends Controller
 {
     public function index(){
-        return view('admin.index')
+        return view('admin.index');
     }
 }
