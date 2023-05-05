e7e2c0bfc0f202a4febf2ab4e1a58393b17385ea
PatrickNthiwa <pwnthiwa@gmail.com>
2023-05-05 19:11:02 +0300

    "n"

--- app/Http/Middleware/Admin.php
+++ app/Http/Middleware/Admin.php
@@ -17,7 +17,7 @@ class Admin
     public function handle(Request $request, Closure $next)
     {
         if(!auth()->check() || !auth()->user()->is_admin==1){
-            abort(403)
+            abort(403);
         }
         return $next($request);
     }
