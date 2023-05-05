ceb4a47fe0907d2f961d1da312c8735cb07a001a
PatrickNthiwa <pwnthiwa@gmail.com>
2023-05-05 19:27:37 +0300

    "H"

--- routes/web.php
+++ routes/web.php
@@ -31,7 +31,7 @@ Route::middleware('auth')->group(function () {
 
 
 Route::middleware('auth','admin')->name('admin.')->prefix('admin')->group(function(){
-    Route::get('/',[App\HttAdminController::class,'index'])->name('index');
+    Route::get('/',[App\Http\AdminController::class,'index'])->name('index');
 });
 
 
